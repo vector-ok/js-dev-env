e53013babe95337c3e1b23c197ae898132ea5749
Victor <vikzy2rhyme@yahoo.com>
2019-07-26 08:28:13 +0100

    on Extracting and minifying css

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,5 @@
 import path from 'path';
-//import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 export default {
   debug: true,
@@ -15,11 +15,11 @@ export default {
     filename: 'bundle.js'
   },
   plugins: [
-    // // Create HTML file that includes reference to bundled JS.
-    // new HtmlWebpackPlugin({
-    //   template: 'src/index.html',
-    //   inject: true
-    // })
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: true
+    })
   ],
   module: {
     loaders: [
